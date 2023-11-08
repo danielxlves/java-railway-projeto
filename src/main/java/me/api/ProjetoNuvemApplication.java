903654ec85ada24c6107c6c0a7e73012a068ccc2
daniel <daniel.nascimento.702@ufrn.edu.br>
2023-11-08 16:20:55 -0300

    Initial commit

---
+++ src/main/java/me/api/ProjetoNuvemApplication.java
@@ -0,0 +1,13 @@
+package me.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/api/ProjetoNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
