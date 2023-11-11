8dcc98a48df433fad8d55933505278a503a47467
daniel <daniel.nascimento.702@ufrn.edu.br>
2023-11-11 16:20:32 -0300

    Quinta tentativa de correção do Cors.

--- src/main/java/me/api/ProjetoNuvemApplication.java
+++ src/main/java/me/api/ProjetoNuvemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class ProjetoNuvemApplication {
 
 	public static void main(String[] args) {
